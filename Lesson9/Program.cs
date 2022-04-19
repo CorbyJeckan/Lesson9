80eb63d36979a27301dd1511b87ef1626bad9045
Евгений Савельев <curtisjeckan@mail.ru>
2022-04-19 16:54:34 +0500

    почти доделал

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -47,10 +47,107 @@ namespace FileManager
             do
             {
                 key = Console.ReadKey().KeyChar;
+
+                if (key != 8 && key != 13)
+                    command.Append(key);
+
+                (int currentLeft, int currentTop) = GetCursorPosition();
+
+                if(currentLeft == width - 2)
+                {
+                    Console.SetCursorPosition(currentLeft - 1, top);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(currentLeft - 1, top);
+                }
+                if (key == (char)8)
+                {
+                    if (command.Length > 0)
+                        command.Remove(command.Length - 1, 1);
+                    if (currentLeft >= left)
+                    {
+                        Console.SetCursorPosition(currentLeft, top);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(currentLeft, top);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(currentLeft, top);
+                    }
+                }
             }
             while (key != (char)13);
+            ParseCommandString(command.ToString());
 
 
+        }
+        static void ParseCommandString(string command) 
+        {
+            string [] commandParams = command.ToLower().Split(' ');
+            if (commandParams.Length > 0)
+            {
+                switch(commandParams[0])
+                {
+                    case "cd":
+                        if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
+                        {
+                            
+                            
+                                currentDir = commandParams[1];
+                            
+                        }
+
+                        break;
+                    case "ls":
+                        if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
+                        {
+                            if (commandParams.Length > 3 && commandParams[2] == "-p" && int.TryParse(commandParams[3], out int n))
+                            {
+                                DrawTree(new DirectoryInfo(commandParams[1]), n);
+                            }
+                            else
+                            {
+                                DrawTree(new DirectoryInfo(commandParams[1]), 1);
+                            }
+                        }
+                        break;
+                }
+            }
+            UpdateConsole();
+        }
+       
+        
+        /// <summary>
+        /// отрисовать дерево каталогов
+        /// </summary>
+        /// <param name="dir">директория</param>
+        /// <param name="page">страница</param>
+        static void DrawTree(DirectoryInfo dir, int page)
+        {
+           StringBuilder tree = new StringBuilder();
+            GetTree(tree, dir, "", true);
+
+            //todo вывести дерево на экран
+        }
+
+        static void GetTree(StringBuilder tree, DirectoryInfo dir, string indent, bool lastDirectory)
+        {
+            tree.Append(indent);
+            if (lastDirectory)
+            {
+                tree.Append("└");
+                indent += " ";
+            }
+            else
+            {
+                tree.Append("├");
+                indent += "│";
+            }
+
+            tree.Append($"{dir.Name}\n");
+
+            DirectoryInfo[] subDirects = dir.GetDirectories();
+            for (int i = 0; i < subDirects.Length; i++)
+                GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1);
         }
 
         /// <summary>
@@ -65,6 +162,7 @@ namespace FileManager
         {
             DrawWindow(x, y, width, height);
             Console.SetCursorPosition(x + 1, y + height / 2);
+            Console.Write($"{dir}>");
         }
 
         /// <summary>
