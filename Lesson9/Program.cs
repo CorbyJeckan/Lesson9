68a0fe46d88b72cab93d9f752349633ae8abed57
Евгений Савельев <curtisjeckan@mail.ru>
2022-04-19 15:04:49 +0500

    отрисовал рамки, установил курсор в командную строку

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,97 @@ namespace FileManager
 {
     internal class  Program
     {
+        const int WINDOW_HEIGHT = 30;
+        const int WINDOW_WIDTH = 120;
+        private static string currentDir = Directory.GetCurrentDirectory();
+
         static void Main(string[] args)
         {
             Console.Title = "FileManager";
-            
+
+            Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+            Console.SetBufferSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+            DrawWindow(0, 0, WINDOW_WIDTH, 18);
+            DrawWindow(0, 18, WINDOW_WIDTH, 8);
+
+            UpdateConsole();
+
             Console.ReadKey(true);
         }
 
+        static void UpdateConsole()
+        {
+            DrawConsole(currentDir, 0, 26, WINDOW_WIDTH, 3);
+            ProcessEnterCommand(WINDOW_WIDTH);
+        }
+
+        static (int left, int top) GetCursorPosition()
+        {
+            return(Console.CursorLeft, Console.CursorTop);
+        }
+
+        static void ProcessEnterCommand(int width)
+        {
+            (int left, int top) = GetCursorPosition();
+            StringBuilder command = new StringBuilder();
+            char key;
+            do
+            {
+                key = Console.ReadKey().KeyChar;
+            }
+            while (key != (char)13);
+
+
+        }
+
+        /// <summary>
+        /// Отрисовать консоль
+        /// </summary>
+        /// <param name="dir">Текущая директория</param>
+        /// <param name="x">Начальная позиция по оси X</param>
+        /// <param name="y">Начальная позиция по оси Y</param>
+        /// <param name="width">Ширина</param>
+        /// <param name="height">Высота</param>
+        static void DrawConsole(string dir, int x, int y, int width, int height)
+        {
+            DrawWindow(x, y, width, height);
+            Console.SetCursorPosition(x + 1, y + height / 2);
+        }
+
+        /// <summary>
+        /// Отрисовать окно
+        /// </summary>
+        /// <param name="x">Начальная позиция по оси X</param>
+        /// <param name="y">Начальная позиция по оси Y</param>
+        /// <param name="width">Ширина</param>
+        /// <param name="height">Высота</param>
         static void DrawWindow(int x, int y, int width, int height)
         {
+            Console.SetCursorPosition(x, y);
+            //header - шапка
+            Console.Write("╔");
+            for (int i = 0; i < width - 2; i++) // 2 - уголка
+                Console.Write("═");
+            Console.Write("╗");
 
+            Console.SetCursorPosition(x, y + 1);
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.Write("║");
+                for (int j = x + 1; j < x + width - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("║");
+            }
+
+            // footer - Подвалд
+            Console.Write("╚");
+            for (int i = 0; i < width - 2; i++) // 2 - уголка
+                Console.Write("═");
+            Console.Write("╝");
+            Console.SetCursorPosition(x, y);
         }
     }
 }
